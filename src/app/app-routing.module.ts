3d70259ab401e8a6b2f81b3ca4c1114de45cffc2
Falathar <falathar@outlook.fr>
2018-08-23 14:55:44 +0200

    Add Pokemon map route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
 import {RegisterComponent} from './register/register.component';
+import {PokemonMapComponent} from './pokemon-map/pokemon-map.component';
 
 const routes: Routes = [
   {path: '', component: RegisterComponent},
-  {path: 'pokemons', component: PokemonListComponent}
+  {path: 'pokemons', component: PokemonListComponent},
+  {path: 'map', component: PokemonMapComponent}
 ];
 
 @NgModule({
